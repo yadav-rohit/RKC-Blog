74b54442e626314850dad8988f4bf399395f0ba3
yadav-rohit <49914319+yadav-rohit@users.noreply.github.com>
2022-06-06 10:15:51 +0530

    css issue

--- Components/blogCard.js
+++ Components/blogCard.js
@@ -13,22 +13,22 @@ const styles = {
 
 function BlogPost({ title, author, coverPhoto, datePublished, slug }) {
   return (
-    <div className={styles.card}>
+    <div className="flex flex-col  w-[] md:w-1/2 col-span-1 rounded-[2em] shadow-xl overflow-hidden m-2 bg-indigo-100 h-fit mx-6">
       <Link href={`/posts/${slug}`}>
-        <div className={styles.imgContainer}>
+        <div className="h-[35vh] overflow-hidden">
           <img  className="shadow-xl" src={coverPhoto.url} alt="" />
         </div>
       </Link>
       <div className={styles.text}>
         <h1 className="flex font-bold  text-xl mx-7 justify-center uppercase items-center">{title}
         </h1>
-        <div className={styles.details}>
-          <div className={styles.author}>
+        <div className="flex flex-row justify-between items-center mx-7 ">
+          <div className="flex flex-row p-2  text-sm text-zinc-600 items-center  justify-between font-bold font-mono">
             <Image width="30vh" height="30vh" className=" drop-shadow-sm" style={ {borderRadius: "50%" }} src={author.avatar.url} alt={author.name} />
             <h3> &nbsp; {author.name} </h3>
             {/* <h3>{datePublished}</h3> */}
           </div>
-          <div className={styles.date}>
+          <div className="font-bold font-mono text-sm text-zinc-600 mx-7">
           <h3>{moment(datePublished).format("MMMM d, YYYY")}</h3>
             {/* <h3>{moment(datePublished).format("MMMM d, YYYY")}</h3> */}
           </div>
